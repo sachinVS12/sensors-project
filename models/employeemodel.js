5df76b2c6981fbb6ec95dfb652c8b630492f33e5
Sachin V S <vs1sachi12@gmail.com>
2026-01-07 18:01:36 +0530

    message 12

--- models/employeemodel.js
+++ models/employeemodel.js
@@ -66,3 +66,44 @@ const employeeSchema = new mongoose.Schema(
             timestamps: true,
         }
 );
+
+// pre save midddlware has password saving befor database 
+employeeSchema("save", async function (next) {
+    if(!this.isModified("password")) {
+        return next();
+    }
+const salt = await bcrypt.gensalt(10);
+this.password = await bcrypt.hash(this.password, salt);
+next();
+});
+  
+
+//jwt token verify signedup and loggedin
+employeeSchema.methods.getToken = function () {
+     return jwt_sign(
+        {
+            id: this._id,
+            name: this.name,
+            email: this.email,
+            password: this.password,
+            role: this.role,
+            assigneddigitalmeters: this.assigneddigitalmeters,
+        },
+        process.env.JWT_SECRET,
+        {
+            expireIn: "3d",
+        }
+     );
+};
+
+
+// method to enterpaswor to exiating password in database
+employeeSchema.methods.verify = async function (enterpassword) {
+    return await bcrypt.compare(this.password, enterpassword);
+};
+
+// create manger model
+const employee = mongoose.model("employee", employeeSchema);
+
+// exports in model
+module.exports= employee;
