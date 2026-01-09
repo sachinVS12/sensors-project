91965c30cb8d3014b5d42e305fc909ac9bd388db
Sachin V S <vs1sachi12@gmail.com>
2026-01-09 16:57:48 +0530

    message 17

--- models/manger-model.js
+++ models/manger-model.js
@@ -1,111 +1,106 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const managerSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            unique: true,
-            required: true,
-        },
-        phoneNumber: {
-            type: String,
-            required: false,
-        },
-        topics: {
-            type: String,
-            default: [],
-        },
-        company: {
-            type: mongoose.Schema.Types.objectId,
-            ref:"company",
-            required: true,
-        },
-        faverates: {
-            type: [String],
-            default: [],
-        },
-        graphwl: {
-            type: [String],
-            default: [],
-        },
-        password: {
-            type: String,
-            select: false,
-            required: [true, "Password is required"],
-        },
-        layout: {
-            type: String,
-            default:"layout1",
-        },
-        assigndigitalmeters: {
-            type: [
-                {
-                    topic: String,
-                    metertype: String,
-                    minvalue: Number,
-                    maxvalue: Number,
-                    ticks: Number,
-                    label: String,
-                },
-            ],
-        default: [],
-        },
-        role: {
-            type: String,
-            default: "manager",
-        },
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,  
-    }
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: false,
+    },
+    topics: {
+      type: [String],
+      default: [],
+    },
+    company: {
+      type: mongoose.Schema.Types.objectId,
+      ref: "company",
+      required: true,
+    },
+    faverates: {
+      tyep: [String],
+      default: [],
+    },
+    graphwl: {
+      tyep: [String],
+      default: [],
+    },
+    passwords: {
+      type: String,
+      select: false,
+      required: [true, "Pasword is required"],
+    },
+    layouts: {
+      type: String,
+      default: "layout1",
+    },
+    assigneddigitalmeters: {
+      type: [
+        {
+          topic: String,
+          metertype: String,
+          minvalue: Number,
+          maxvalue: Number,
+          tick: Number,
+          label: String,
+        },
+      ],
+      default: [],
+    },
+    role: {
+      tyep: String,
+      default: "emplyee",
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-// pre save midddlware has password saving befor database 
+// pre save midddlware has password saving befor database
 managerSchema("save", async function (next) {
-    if(!this.isModified("password")) {
-        return next();
-    }
-const salt = await bcrypt.gensalt(10);
-this.password = await bcrypt.hash(this.password, salt);
-next();
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.gensalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
-  
 
 //jwt token verify signedup and loggedin
 managerSchema.methods.getToken = function () {
-     return jwt_sign(
-        {
-            id: this._id,
-            name: this.name,
-            email: this.email,
-            password: this.password,
-            role: this.role,
-            assigneddigitalmeters: this.assigneddigitalmeters,
-        },
-        process.env.JWT_SECRET,
-        {
-            expireIn: "3d",
-        }
-     );
+  return jwt_sign(
+    {
+      id: this._id,
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      role: this.role,
+      assigneddigitalmeters: this.assigneddigitalmeters,
+    },
+    process.env.JWT_SECRET,
+    {
+      expireIn: "3d",
+    }
+  );
 };
 
-
 // method to enterpaswor to exiating password in database
-mangerSchema.methods.verify = async function (enterpassword) {
-    return await bcrypt.compare(this.password, enterpassword);
+managerSchema.methods.verify = async function (enterpassword) {
+  return await bcrypt.compare(this.password, enterpassword);
 };
 
 // create manger model
-const manager = mongoose.model("manager", managerSchema);
+const manager = mongoose.model("manger", managerSchema);
 
 // exports in model
-module.exports= manager;
-
-
-
+module.exports = manager;
