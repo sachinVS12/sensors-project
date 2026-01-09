a1f9266765fb908ea3c80795a4e0a4d3e22b03e7
Sachin V S <vs1sachi12@gmail.com>
2026-01-09 17:44:24 +0530

    message 18

---
+++ models/supervisormodel.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const supervisors = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: false,
+    },
+    topics: {
+      type: [String],
+      default: [],
+    },
+    company: {
+      type: mongoose.schema.Types.objectId,
+      ref: "company",
+      required: true,
+    },
+    favorates: {
+      type: [string],
+      default: [],
+    },
+    graphwl: {
+      type: [string],
+      default: [],
+    },
+    password: {
+      type: string,
+      select: false,
+      required: [true, "Password is required"],
+    },
+    layout: {
+      type: string,
+      default: "layout1",
+    },
+    assigneddigitalmeters: {
+      type: [
+        {
+          topic: String,
+          metertype: String,
+          minvalue: Number,
+          maxvalue: Number,
+          tick: Number,
+          lable: String,
+        },
+      ],
+      default: [],
+    },
+    role: {
+      type: string,
+      default: "supervisors",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
