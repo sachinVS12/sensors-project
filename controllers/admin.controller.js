14f7fdd313015354c13a9303f2271b1b3f75adc8
Sachin V S <vs1sachi12@gmail.com>
2026-01-17 15:19:36 +0530

    message 43

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -18,3 +18,39 @@ exports.createUser = async (req, res) => {
 
   res.json({ message: `${role} created successfully` });
 };
+
+//login
+const login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await user.findone({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorResponse("Invalid Credentials", 401));
+  }
+  const isMatch = await user.verifypass(password);
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid Credentials", 401));
+  }
+  const token = await getToken();
+  res.status(200).json({
+    success: true,
+    token,
+  });
+});
+
+//admin
+const admin = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  const admin = await admin.findone({ email }).select("+password");
+  if (!admin) {
+    return next(new ErrorResponse("Invalid Credentials", 401));
+  }
+  const isMatch = await admin.verifypass(password);
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+  const token = await admin.getToken();
+  res.status(200).json({
+    success: true,
+    token,
+  });
+});
